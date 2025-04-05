ddb3dbacacf371795594fc21a5529b71a74baf5f
Kris-Mat <matsila.krista@gmail.com>
2025-04-05 13:11:52 +0200

    added conversion tests from hexadecimal to decimal and vice versa

---
+++ tests/conversions.test.js
@@ -0,0 +1,18 @@
+// tests to check that the conversions from hexadecimal to decimal and back vice versa are correct
+
+test('hexadecimal value FF to decimal should return 255', () =>{
+
+});
+
+test('hexadecimal value -FF to decimal should return -255', () =>{
+
+});
+
+test('decimal value -175 to hexadecimal should return -AF', () =>{
+
+});
+
+test('decimal value 175 to hexadecimal should return AF', () =>{
+
+});
+
