e14b6c5ad3ee8c130ccab0860f4136714c3f7ca7
Kris-Mat <matsila.krista@gmail.com>
2025-04-06 15:33:46 +0200

    made test names more specific for IO tests

--- tests/input_output_validation.test.js
+++ tests/input_output_validation.test.js
@@ -2,79 +2,76 @@ const {add, subtract, divide, multiply } = require('../calculator_functions/arit
 const {isValidInputLength, isValidOutputLength, isValidHexadecimalValue, isValueNegative} = require('../calculator_functions/input_output_validation');
 
 // input and output format validation 
-test('return false for invalid number of values input', ()=>{
+test('return false for invalid number of values input (3333)', ()=>{
     expect(isValidInputLength('3333')).toBeFalsy();
 });
 
-test('return true for valid number of values input', ()=>{
+test('return true for valid number of values input (ff)', ()=>{
     expect(isValidInputLength('ff')).toBeTruthy();
 });
 
-test('return true if the output value is 4 digits or less', () =>{
+test('return true if the output value is 4 digits or less (22)', () =>{
     expect(isValidOutputLength('22')).toBeTruthy();
 });
 
-test('return true if the output value is equal to 4 digits long', () =>{
+test('return true if the output value is equal to 4 digits long (444)', () =>{
     expect(isValidOutputLength('4444')).toBeTruthy();
 });
 
-test('return false if the output value is more than 4 digits long', () =>{
+test('return false if the output value is more than 4 digits long (44445)', () =>{
     expect(isValidOutputLength('44445')).toBeFalsy();
 });
 
-test('returns false for values that are not negative', () =>{
+test('returns false for values that are not negative (4F)', () =>{
     expect(isValueNegative('4F')).toBeFalsy();
 });
 
-test('returns true for values that are negative', () =>{
+test('returns true for values that are negative (-47)', () =>{
     expect(isValueNegative('-4F')).toBeTruthy();
 });
 
-test('returns true for only hexadecimal values input', () =>{
+test('returns true for only hexadecimal values input (4F23)', () =>{
     expect(isValidHexadecimalValue('4F23')).toBeTruthy();
 });
 
-test('returns true for only hexadecimal values input', () =>{
+test('returns true for only hexadecimal values input (4F)', () =>{
     expect(isValidHexadecimalValue('4F')).toBeTruthy();
 });
 
 
-test('returns true for only hexadecimal values input despite lower case letter', () =>{
+test('returns true for only hexadecimal values input despite lower case letter (4f23)', () =>{
     expect(isValidHexadecimalValue('4f23')).toBeTruthy();
 });
 
-test('returns true for only hexadecimal values input despite negative number', () =>{
+test('returns true for only hexadecimal values input despite negative number (-4f)', () =>{
     expect(isValidHexadecimalValue('-4f')).toBeTruthy();
 });
 
-test('returns false for non-hexadecimal values input', () =>{
+test('returns false for non-hexadecimal values input (R5FA@)', () =>{
     expect(isValidHexadecimalValue('R5FA@')).toBeFalsy();
 });
 
-test('returns false for non-hexadecimal values input despite lower case letter', () =>{
+test('returns false for non-hexadecimal values input despite lower case letter (r5af@)', () =>{
     expect(isValidHexadecimalValue('r5af@')).toBeFalsy();
 });
 
 // checking that the arithmetic operations have an output that is only hexadecimal
-test('returns a positive hexadecimal value as an answer for the addition', () =>{
+test('returns a positive hexadecimal value as an answer for the addition (21, 2)', () =>{
     expect(add('21', '2')).toMatch(/^[0-9A-Fa-f]+$/);
 });
 
-test('returns a positive hexadecimal value as an answer for the subtraction', () =>{
+test('returns a positive hexadecimal value as an answer for the subtraction (21, 2)', () =>{
     expect(subtract('21', '2')).toMatch(/^[0-9A-Fa-f]+$/);
 });
 
-test('returns null because the answer has  a fraction/remainder', () =>{
+test('returns null because the answer has  a fraction/remainder (21, 2)', () =>{
     expect(divide('21', '2')).toBe(null);
 });
 
-test('returns a positive hexadecimal value as an answer for the division', () =>{
+test('returns a positive hexadecimal value as an answer for the division (21, 21)', () =>{
     expect(divide('21', '21')).toMatch(/^[0-9A-Fa-f]+$/);
 });
 
-test('returns a positive hexadecimal value as an answer for the multiplication', () =>{
+test('returns a positive hexadecimal value as an answer for the multiplication (21, 2)', () =>{
     expect(multiply('21', '2')).toMatch(/^[0-9A-Fa-f]+$/);
 });
-
-
-
